b3e8724f386d87d6f64e3ebf250a800399b8ffb8
Nikhil Nair <43491351+NickNair@users.noreply.github.com>
2021-08-23 16:16:13 +0530

    Update artpark_sim_entrypoint.sh

--- artpark_docker/artpark_sim_entrypoint.sh
+++ artpark_docker/artpark_sim_entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 source /opt/ros/noetic/setup.bash
 source /artpark_workspace/install/setup.bash
 source /artpark_workspace/devel/setup.bash
+chmod +x artpark_workspace/src/GigaRoboticsArtpark/apbot_nav/scripts/*
+chmod +x artpark_workspace/src/GigaRoboticsArtpark/apbot_description/scripts/*
 
 exec "$@"
