2ef2d44a47a493a8185807f665721082497d9302
Nikhil Nair <43491351+NickNair@users.noreply.github.com>
2021-08-23 16:15:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 sudo xhost +local:root
 cd artpark_docker
-sudo docker build . -t artpark
-docker rm -f artpark_workspace_sim_container
+sudo chmod +x artpark_sim_entrypoint.sh run_artpark_sim.bash
+sudo docker build . -t artpark --no-cache=true
+sudo docker rm -f artpark_workspace_sim_container
 sudo ./run_artpark_sim.bash
 
